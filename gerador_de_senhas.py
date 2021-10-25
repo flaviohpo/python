3282222552d33566b32a79f18424b5831420e5af
Flavio Henrique Pereira de Oliveira <flaviohpo@yahoo.com>
2021-10-24 23:40:09 -0300

    adicionado gerador de senhas

---
+++ gerador_de_senhas.py
@@ -0,0 +1,40 @@
+""" This module has functions to generate random passwords """
+import random
+import pyperclip
+
+from pylint import epylint as lint
+
+(pylint_stdout, pylint_stderr) = lint.py_run("gerador_de_senhas.py", return_std=True)
+print(pylint_stdout.read())
+print(pylint_stderr.read())
+
+# gerador de senhas
+def gera_senha(
+    programa,
+    n_digits=8,
+    gravar_arquivo=False,
+    copy_to_clipboard=False,
+    diretorio="/media/flaviohpo/01D7B89E9D82D660/Users/user/Desktop/",
+):
+    """
+    This function returns an 25 character password
+    it may copy the password to clipboard
+    and also can create a text file with the password
+    somewhere.
+    """
+    senha = ""
+    for num in range(0, n_digits):
+        senha += chr((int(random.SystemRandom(num).random() * 1000) % 90) + 35)
+    print(f"A senha gerada para {programa} foi: {senha}")
+    if copy_to_clipboard is True:
+        pyperclip.copy(senha)
+    print("A senha foi copiada para o clipboard")
+    if gravar_arquivo is True:
+        print(f"Um arquivo foi criado em {diretorio}")
+        file = open(f"{diretorio}{programa}.txt", "w")
+        file.write(senha)
+        file.close()
+
+
+gera_senha("makerbot", 25, gravar_arquivo=True, copy_to_clipboard=True, diretorio="/home/flaviohpo/Documents/")
+print(gera_senha.__doc__)
--- testes_zip.py
+++ testes_zip.py
@@ -6,3 +6,9 @@ answers = ['lancelot', 'the holy grail', 'blue']
 for q, a in zip(questions, answers):
     print('What is your {0}?  It is {1}.'.format(q, a))
     print(f'What is your {q}?  It is {a}.')
+
+
+def func():
+    pass
+
+print(func())
