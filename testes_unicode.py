5b3e0b9f90d4c9079fdd46018e11956464f422f3
flaviohpo <flaviohpo@yahoo.com>
2021-11-02 23:35:42 -0300

    adicionado testes de unicode

---
+++ testes_unicode.py
@@ -0,0 +1,10 @@
+string_foo = f'oi eu sou uma string foo é ã ä \u0394 1'
+
+string_encoded = string_foo.encode()
+
+print(string_foo)
+
+print(f'tipo da string_decoded = {type(string_encoded)}')
+
+#for b in string_encoded:
+    #print(b)
