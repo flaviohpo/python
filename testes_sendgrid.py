b0363563f3ea84948766a21641b35d8b1cdc98a7
flaviohpo <flaviohpo@yahoo.com>
2022-01-31 21:52:19 -0300

    adicionado teste sendgrid no django project

--- testes_sendgrid.py
+++ testes_sendgrid.py
@@ -3,13 +3,35 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Bcc
+
+# ler um arquivo de anuncio e passar o conteudo do arquivo para o html_content
+# ler o arquivo de endereços e adicionar o to_emails
+# procurar na API limite de emails que da pra adicionar nas mensagens
+# teria alguma classe diferente na api pra mandar email pra um monte de gente
+# ou teria que botar isso num loop?
+# maximo de 1000 recipients
+# https://docs.sendgrid.com/api-reference/mail-send/limitations
+# 
 
 message = Mail(
-    from_email='from_email@example.com',
-    to_emails='flaviohpo@yahoo.com',
+    from_email='contas@ible.com.br',
+    to_emails=['contas@ible.com.br'],
     subject='Sending with Twilio SendGrid is Fun',
     html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
+    message.bcc = [
+        Bcc(
+            email='flaviohpo@yahoo.com',
+            name="Friendly Flavio",
+            p=0
+        ),
+        Bcc(
+            email='flaviohenriquepereiraoliveira@gmail.com',
+            name="Friendlyyyy Flavio",
+            p=0
+        )
+    ]
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
